67530eb83d0469f06aaef652cd4c45a665ef76f5
alibabapour1 <49041775+alibabapour1@users.noreply.github.com>
2024-01-28 21:54:20 +0330

    Add project files.

---
+++ simpleWebAppUsingIdentity/Models/DbContext/MyDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace simpleWebAppUsingIdentity.Models.DbContext
+{
+    public class MyDbContext:IdentityDbContext
+    {
+    }
+}
